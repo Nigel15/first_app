e0c9fc151499874d8399eea7e71e0392d3b2ab4f
nigel <sngcrow@gmail.com>
2013-07-18 15:57:24 -0400

    Initial commit

---
+++ codes/1.rb
@@ -0,0 +1,9 @@
+puts "Is it greater?", 5 > -2
+
+formatter = "%s %s %s %s"
+puts formatter % [
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+]
